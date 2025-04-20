b5b767958fce19ccd12c5c87de60d1618e855ecc
Exploit34 <josefexner456@gmail.com>
2025-04-20 01:07:02 -0500

    modify the vite.config.ts file and add the static.json file and also delete the dashboard file.

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist'
+  server: {
+    middlewareMode: true,
   },
-  
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
